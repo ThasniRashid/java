d14cf3ca6e14c837b693fb429a887e51f483b38c
unknown <thasnirashid>
2022-03-16 10:06:02 +0530

    first commit

---
+++ name.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class name
+{
+	public static void main(String args[])
+	{
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter your first name:");
+		String fname=obj.next();
+		Scanner obj1=new Scanner(System.in);
+		System.out.println("Enter your second name:");
+		String lname=obj1.next();
+		System.out.println(fname+" "+lname);
+	}
+}
